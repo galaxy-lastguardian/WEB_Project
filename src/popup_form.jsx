e8c2186b0b630306b4c1b775653072ec4725cb7a
lastguardian1488 <taras1246@gmail.com>
2022-01-11 18:14:55 +0300

    Настраиваем оторбражение картинок

--- src/popup_form.jsx
+++ src/popup_form.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import "./popup.css"
+// IMAGES ***************************
+import feedbackIcon1 from "./img/feedback-icon1.png";
+import feedbackIcon2 from "./img/feedback-icon2.png";
+import footerPic1 from "./img/footer-pic1.svg";
+import footerPic2 from "./img/footer-pic2.png";
+// IMAGES ***************************
 
 const Modal = ({active, setActive}) => {
     return (
         <div className={active ? "popup_form_overlay active" : "popup_form_overlay"} onClick= {() => setActive(false)}>
             <div className={active ? "popup_form_wrapper active" : "popup_form_wrapper"} onClick = {e => e.stopPropagation()}>
-                <img src="/img/footer-pic1.svg" alt="drupal-coder" className="pic1"/>
-                <img src="/img/footer-pic2.png" alt="drupal-coder" className="pic2"/>
+                <img src={footerPic1} alt="drupal-coder" className="pic1"/>
+                <img src={footerPic2} alt="drupal-coder" className="pic2"/>
                 <div className="container">
                     <span onClick= {() => setActive(false)}>&#10006;</span>
                     <div className="feedback" id="feedback-form">
@@ -20,11 +26,11 @@ const Modal = ({active, setActive}) => {
                                         <p>Срочно нужна поддержка сайта? Ваша команда не успевает справиться самостоятельно или предыдущий подрядчик не справился с работой? Тогда вам точно к нам! Просто оставьте заявку и наш менеджер с вами свяжется!</p>
                                         <div className="elem">
                                             <div className="number">
-                                                <h3><img src="/img/feedback-icon1.png" alt="icon" className="icon"/> 8 800 222-26-73</h3>
+                                                <h3><img src={feedbackIcon1} alt="icon" className="icon"/> 8 800 222-26-73</h3>
                                             </div>
                                             <div className="email">
-                                                <img src="/img/feedback-icon2." alt="icon" className="icon"/>
-                                                <a href="">info@drupal-coder.ru</a>
+                                                <img src={feedbackIcon2} alt="icon" className="icon"/>
+                                                <a href="https://drupal-coder.ru">info@drupal-coder.ru</a>
                                             </div>
                                         </div>
                                     </div>
@@ -48,7 +54,7 @@ const Modal = ({active, setActive}) => {
                                         <div className="col-12">
                                             <div className="checkbox">
                                                 <input type="checkbox" name = "checkbox" id="form-checkbox" className="checkbox_input" required/>
-                                                <label htmlFor="form-checkbox" className="checkbox_label">Отправляя заявку, я даю согласие <a href="">на обработку своих персональных данных</a>.<span>*</span></label>
+                                                <label htmlFor="form-checkbox" className="checkbox_label">Отправляя заявку, я даю согласие <a href="https://drupal-coder.ru">на обработку своих персональных данных</a>.<span>*</span></label>
                                             </div>
                                         </div>
                                         <div className="col-12">
@@ -64,5 +70,3 @@ const Modal = ({active, setActive}) => {
         </div>
     );
 }
-
-export default Modal
